13a7644ff096cf59ccca89c2a15054ed1f6999d6
Aidar <Abdulnagimov-AI@yandex.ru>
2024-12-13 12:54:32 +0500

    финальный проект спринта 16(2)

--- gateway/shareit-gateway/src/main/java/ru/practicum/shareit/exception/NotValidOwner.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.shareit.exception;
-
-public class NotValidOwner extends RuntimeException {
-    public NotValidOwner(String message) {
-        super(message);
-    }
-}
-
--- gateway/shareit-gateway/src/main/java/ru/practicum/shareit/exception/NotValidRequestException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exception;
-
-public class NotValidRequestException extends RuntimeException {
-    public NotValidRequestException(String message) {
-        super(message);
-    }
-}
