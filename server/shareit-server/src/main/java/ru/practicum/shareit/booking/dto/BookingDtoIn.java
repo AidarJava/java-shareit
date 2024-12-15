c3193616633874ed2588250f4a5502660d722035
Aidar <Abdulnagimov-AI@yandex.ru>
2024-12-15 12:11:44 +0500

    добавлено тестовое покрытие2

--- server/shareit-server/src/main/java/ru/practicum/shareit/booking/dto/BookingDtoIn.java
+++ server/shareit-server/src/main/java/ru/practicum/shareit/booking/dto/BookingDtoIn.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.booking.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.FutureOrPresent;
@@ -16,11 +15,9 @@ public class BookingDtoIn {
     Long id;
     @NotNull
     @FutureOrPresent
-   // @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     LocalDateTime start;
     @NotNull
     @Future
-   // @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     LocalDateTime end;
     @NotNull
     Long itemId;
